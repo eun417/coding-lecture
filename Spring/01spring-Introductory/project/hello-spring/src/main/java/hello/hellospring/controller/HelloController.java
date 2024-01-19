ae6eefb703030106b3213010f205070f22cd5fa4
eun417 <126125547+eun417@users.noreply.github.com>
2024-01-19 20:53:12 +0900

    Spring 입문(김영한) - 섹션 1, 2 코드 정리
    섹션1: 프로젝트 환경설정
    섹션2: 스프링 웹 개발 기초

---
+++ Spring/01spring-Introductory/project/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,52 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "Spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        //문자가 요청한 클라이언트에게 그대로 내려감
+        return "hello " + name; //"hello Spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체 넘김 -> JSON 방식으로 데이터를 만들어서 HTTP 응답에 반환(기본 정책)
+    }
+
+    static class Hello {
+        private String name;
+
+        //JavaBean 표준 방식 / 프로퍼티 접근 방식
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
