f3574468bf6cf63820979ec8f0626d43856efe9a
eun417 <126125547+eun417@users.noreply.github.com>
2024-01-21 18:13:46 +0900

    Spring 입문(김영한) - 섹션5 코드 정리
    섹션5: 회원 관리 예제 - 웹 MVC 개발

--- Spring/01spring-Introductory/project/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Spring/01spring-Introductory/project/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,6 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /**
  * @Controller 있으면 MemberController 객체를 생성해서 Spring에 넣어두고, 관리
@@ -42,5 +47,32 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        //페이지 이동
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";    //홈 화면으로 이동
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        //모든 회원 조회
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
